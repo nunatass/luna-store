2024e6eeb6624f478fa486580f4295bcb78f0649
Nuno Andrade <nunatass@gmail.com>
2024-02-20 17:18:06 +0000

    feat: improve seo

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,19 +1,34 @@
-import Head from 'next/head';
+import { Metadata } from 'next';
 
 type SEOProps = {
   pageTitle: string;
 };
 
-export const SEO = ({ pageTitle }: SEOProps) => (
-  <Head>
-    <title>{pageTitle && `${pageTitle} - Luna Store`}</title>
-    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-    <meta name="description" content="You Jewelry store" />
-    <meta name="robots" content="noindex, follow" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <link rel="icon" href="/favicon.png" />
-  </Head>
-);
+export const metadata: Metadata = {
+  title: 'Stella Store',
+  description: 'Stella Store Products',
+  keywords: [
+    'jewelry store',
+    'jewelry',
+    'bracelets',
+    'necklaces',
+    'rings',
+    'earrings',
+    'style',
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    },
+  },
+};
+
+export const SEO = ({ pageTitle }: SEOProps) => {
+  pageTitle;
+  return <></>;
+};
