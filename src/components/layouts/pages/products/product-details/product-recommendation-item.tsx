ef4fb5e09b4042c89a545029e4db602f9685fd75
Nuno Andrade <nunatass@gmail.com>
2024-03-20 18:21:52 +0000

    fix: fix product recomendation item discount price

--- src/components/layouts/pages/products/product-details/product-recommendation-item.tsx
+++ src/components/layouts/pages/products/product-details/product-recommendation-item.tsx
@@ -160,9 +160,11 @@ export const ProductRecommendationItem = ({
                 }
               </span>
 
-              <span className="sm:font-base w-full text-right text-sm text-xs text-gray-600  line-through transition-all duration-300 ease-in-out sm:mb-2">
-                ${formatPrice(product.prices[0].value)}
-              </span>
+              {product.prices[0].discount > 0 && (
+                <span className="sm:font-base w-full text-right text-sm text-xs text-gray-600  line-through transition-all duration-300 ease-in-out sm:mb-2">
+                  ${formatPrice(product.prices[0].value)}
+                </span>
+              )}
             </div>
           </div>
         </div>
