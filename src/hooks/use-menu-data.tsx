dbc9c7afd417a7b33cebad05d0ed21f787e9bd36
Nuno Andrade <nunatass@gmail.com>
2024-03-21 15:11:10 +0000

    fix: remove sharp

--- src/hooks/use-menu-data.tsx
+++ src/hooks/use-menu-data.tsx
@@ -88,7 +88,7 @@ export function useMenuData() {
                   title: 'All Products',
                   link: `/products`,
                 },
-                ...categories?.slice(0, 4).map((category: Category) => ({
+                ...(categories ?? []).slice(0, 4).map((category: Category) => ({
                   title: category.name,
                   link: `/products?category=${category.name.toLocaleLowerCase()}`,
                 })),
