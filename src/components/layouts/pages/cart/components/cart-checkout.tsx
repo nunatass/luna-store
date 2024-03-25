ea251b6271fe639435f15da9a981b25bff5ad3a3
Nuno Andrade <nunatass@gmail.com>
2024-03-25 14:08:22 +0000

    fix: fix checkout form size

--- src/components/layouts/pages/cart/components/cart-checkout.tsx
+++ src/components/layouts/pages/cart/components/cart-checkout.tsx
@@ -7,7 +7,7 @@ export const CartCheckout = () => {
   const { getTotal } = useCart();
   const { totalWithDiscount } = getTotal();
   return (
-    <div className="flex h-96 w-full min-w-80 flex-col gap-4 divide-y-[1px] bg-white p-6 shadow-sm lg:max-w-80">
+    <div className="flex h-max w-full min-w-80 flex-col gap-4 divide-y-[1px] bg-white p-6 shadow-sm lg:max-w-80">
       <div className="flex h-max items-center justify-between text-xl font-medium">
         <span className="">Subtotal</span>
         <span className="">${formatPrice(totalWithDiscount)}</span>
