0070db09ad1a85b29fd496355a2942c99d31a11c
Nuno Andrade <nunatass@gmail.com>
2024-04-01 10:30:43 +0100

    fix: fix home banner

--- src/components/layouts/pages/home/jewelry-banner.tsx
+++ src/components/layouts/pages/home/jewelry-banner.tsx
@@ -9,7 +9,7 @@ const animateButton = {
   initial: { opacity: 0, scale: 0 },
   animate: { opacity: 1, scale: 1 },
   exit: { opacity: 0, scale: 0 },
-  transition: { duration: 0.9, ease: [0.34, 1.56, 0.64, 1] },
+  transition: { duration: 0.6, ease: [0.34, 1.56, 0.64, 1] },
 };
 
 export const JewelryBanner = () => {
@@ -17,16 +17,14 @@ export const JewelryBanner = () => {
 
   return (
     <div className=" relative h-screen w-screen overflow-hidden">
-      <section className="relative h-full w-full bg-[#AB9774] bg-[url('/banner.jpeg')] bg-center bg-no-repeat bg-cover">
+      <section className="relative h-full w-full bg-[#AB9774] bg-[url('/banner.jpeg')] bg-cover bg-center bg-no-repeat">
         <div className="button absolute left-1/2 top-1/2 z-10 -mt-16 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-2 text-white">
-          <div className="hidden md:block">
-            <div className="text-2xl font-bold md:text-5xl">
-              BE YOU, WEARING US.
-            </div>
+          <div className="text-2xl font-bold md:text-5xl">
+            BE YOU, WEARING US.
           </div>
         </div>
 
-        <div className="absolute bottom-40 left-1/2 z-30 -translate-x-1/2 sm:bottom-16 md:left-20 md:translate-x-0">
+        <div className="absolute bottom-40 left-1/2 -translate-x-1/2 sm:bottom-16 md:left-20 md:translate-x-0">
           <AnimatePresence>
             {!sticky && (
               <motion.div {...animateButton}>
