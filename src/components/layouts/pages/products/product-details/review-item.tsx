4d2435e8707681d7bb47fcb4b356682ee36d0433
Nuno Andrade <nunatass@gmail.com>
2024-04-29 19:07:17 +0100

    fix: item review size

--- src/components/layouts/pages/products/product-details/review-item.tsx
+++ src/components/layouts/pages/products/product-details/review-item.tsx
@@ -9,8 +9,8 @@ type ReviewItemProps = {
 export const ReviewItem = ({ review }: ReviewItemProps) => {
   const { comment, createdAt, rating, user } = review;
   return (
-    <div className="flex h-40 items-center gap-2 bg-white px-4 py-2">
-      <div className="flex flex-col  gap-2">
+    <div className="flex min-h-40 items-center gap-2 bg-white px-4 py-4">
+      <div className="flex flex-col gap-2">
         <Rating
           allowFraction
           size={16}
