00125f358b2194da000e2ca7a0c29fc05a3e682c
Nuno Andrade <nunatass@gmail.com>
2024-05-20 13:36:57 +0100

    feat: change banner copy

--- src/components/layouts/headers/discount-banner.tsx
+++ src/components/layouts/headers/discount-banner.tsx
@@ -12,18 +12,18 @@ export const DiscountBanner = () => {
   const { symbol } = useCurrency();
   return (
     <Slider
-      width="450px"
+      width="500px"
       duration={15}
       pauseOnHover={false}
       blurBorders={false}
       className="h-20 border-0"
     >
-      <Slider.Slide className=" flex h-10 items-center justify-center gap-2 border-2 border-[#ffcbcf] bg-[#ffcbcf] text-black">
-        <DeliveryIcon className="w-8 w-8 text-white" />
+      <Slider.Slide className="flex h-10 w-full items-center justify-center gap-2 border-2 border-[#ffcbcf] bg-[#ffcbcf] text-black">
+        <DeliveryIcon className="w-8 text-white" />
         {`Free Shipping over ${symbol}${freeShippingThreshold / 100}`}
       </Slider.Slide>
-      <Slider.Slide className="flex  h-10 items-center justify-center gap-2 border-2 border-[#ffcbcf] bg-[#ffcbcf] text-black">
-        <DiscountIcon className="w-6 w-6" />
+      <Slider.Slide className="flex h-10 w-full items-center justify-center gap-2 border-2 border-[#ffcbcf] bg-[#ffcbcf] text-black">
+        <DiscountIcon className="w-6" />
         {`Summer Sales - BUY 2, GET 1 FREE`}
       </Slider.Slide>
     </Slider>
--- src/components/layouts/pages/home/jewelry-banner.tsx
+++ src/components/layouts/pages/home/jewelry-banner.tsx
@@ -13,8 +13,13 @@ export const JewelryBanner = () => {
               ★★★★★ LOVED BY 150,000+ CLIENTS
             </div>
           </div>
-          <h2 className="my-8 text-center font-americana text-5xl font-bold md:text-7xl">
-            Buy 2, Get 1 Free!
+          <h2 className="my-8 flex flex-col items-center justify-center text-center font-americana uppercase">
+            <span className="text-2xl font-bold sm:text-3xl md:text-5xl">
+              Waterproof Jewelry
+            </span>
+            <span className="text-xs font-bold sm:text-lg md:text-2xl">
+              you will never have to take off
+            </span>
           </h2>
           <div className="text-[13px] font-light text-white md:text-base">
             Be You, wearing US.
